0c3aa2f37ccc71ddef1599b55920dd9b61066e3d
Nick <njhuemmer@gmail.com>
2022-05-02 13:29:10 -0400

    update the readme, change counter component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ function WelcomeMessage({ children }) {
   return <p>{children}</p>
 }
 
+// deconstructed state passed in as the arguments
+const Counter = ({ count, setCount }) => {
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <WelcomeMessage > Hello Everyone, Welcome to React! </WelcomeMessage>
-        <p>{count}</p>
-        <button onClick={() => setCount(count + 1)}>Click Me</button>
+        <WelcomeMessage > It's time to really learn React! </WelcomeMessage>
+        {/* // State is contained within the React component, but the functions are called here. */}
+        <Counter count={count} setCount={setCount} />
       </header>
     </div>
   );
